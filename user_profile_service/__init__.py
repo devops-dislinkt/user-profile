0ab1b5f72754edf7783245b7ba866480494765f9
specnazm <laketic.milena98@gmail.com>
2022-05-27 18:02:26 +0200

    Changed init flask app

--- user_profile_service/__init__.py
+++ user_profile_service/__init__.py
@@ -1,19 +1,23 @@
 from flask import Flask
-
+from kafka import KafkaConsumer
 from .models import db
 from . import config
 
-def create_app():
-    flask_app = Flask(__name__)
-    flask_app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
-    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    flask_app.app_context().push()
-    db.init_app(flask_app)
-    db.create_all()
 
-    from user_profile_service.views import profile_bp
 
-    flask_app.register_blueprint(profile_bp, url_prefix='/api')
+#
+# def create_app():
+flask_app = Flask(__name__)
 
-    return flask_app
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
+flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+flask_app.app_context().push()
+db.init_app(flask_app)
+db.create_all()
+
+from user_profile_service.views import profile_bp
+
+flask_app.register_blueprint(profile_bp, url_prefix='/api')
+
+
 
