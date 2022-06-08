55b80454963b7160db5bf956a89de155b5a7ed22
specnazm <laketic.milena98@gmail.com>
2022-06-09 01:06:29 +0200

    Changed config file

--- user_profile_service/config.py
+++ user_profile_service/config.py
@@ -1,15 +1,15 @@
 import os
 
-user = os.environ["DATABASE_USERNAME"]
-password = os.environ["DATABASE_PASSWORD"]
-host = os.environ["DATABASE_DOMAIN"]
-database = os.environ["DATABASE_SCHEMA"]
-port = os.environ["DATABASE_PORT"]
-secret_key = os.environ["FLASK_SECRET_KEY"]
+user = os.environ["DATABASE_USERNAME"] if "DATABASE_USERNAME" in os.environ else "postgres"
+password = os.environ["DATABASE_PASSWORD" ] if "DATABASE_PASSWORD" in os.environ else "postgres"
+host = os.environ["DATABASE_DOMAIN"] if "DATABASE_DOMAIN" in os.environ  else "localhost"
+database = os.environ["DATABASE_SCHEMA"] if "DATABASE_PASSWORD" in os.environ  else "postgres"
+port = os.environ["DATABASE_PORT"] if "DATABASE_PORT" in os.environ else 5432
+secret_key = os.environ["FLASK_SECRET_KEY"] if "FLASK_SECRET_KEY" in os.environ else "secret"
 
 DATABASE_CONNECTION_URI = (
     f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
 )
 
-KAFKA_1 = os.environ["KAFKA1"]
-KAFKA_TOPIC = os.environ["KAFKA_TOPIC"]
+KAFKA_1 = os.environ["KAFKA1"] if "KAFKA1" in os.environ else "none"
+KAFKA_TOPIC = os.environ["KAFKA_TOPIC"] if "KAFKA_TOPIC" in os.environ else "default_topic"
