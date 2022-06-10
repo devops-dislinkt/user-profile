a6918132e9a80eb3e5187cf03ab08a67676701ff
Mitar Perovic <perovicmitar@uns.ac.rs>
2022-06-10 17:47:49 +0200

    feat:added tracing with opentelemetry

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from user_profile_service import create_app, create_kafka_thread
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
 
 if __name__ == '__main__':
     app = create_app()
+    FlaskInstrumentor().instrument_app(app)
     create_kafka_thread(app)
     app.run(debug=True, host="0.0.0.0")
