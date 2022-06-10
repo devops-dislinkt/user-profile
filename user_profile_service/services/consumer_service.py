c62a79029c0dfe069720ddd979ec8b22c4c7dc92
specnazm <52412989+specnazm@users.noreply.github.com>
2022-06-10 02:13:05 +0200

    Workflow (#16)
    * Linting project with black
    
    * Added Sonar Cloud check
    
    * Corrected typo
    
    * Changed config file
    
    * Lint
    
    * Missing argument in test added
    
    * Added Sonar Cloud properties file
    
    * Typo in config
    
    * Removed db init from unit tests
    
    * Added integration test
    
    * Lint

--- user_profile_service/services/consumer_service.py
+++ user_profile_service/services/consumer_service.py
@@ -6,6 +6,7 @@ from user_profile_service import config
 from . import profile_service
 from flask import current_app, Flask
 
+
 class ConsumerThread(threading.Thread):
     def __init__(self, app: Flask):
         threading.Thread.__init__(self)
@@ -17,26 +18,35 @@ class ConsumerThread(threading.Thread):
 
     def run(self):
         self.create_topic()
-        consumer = KafkaConsumer(bootstrap_servers=config.KAFKA_1, value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+        consumer = KafkaConsumer(
+            bootstrap_servers=config.KAFKA_1,
+            value_deserializer=lambda m: json.loads(m.decode("utf-8")),
+        )
         consumer.subscribe(config.KAFKA_TOPIC)
         for message in consumer:
             with self.app.app_context():
-                current_app.logger.info('Received message with username : %s',message.value["username"])
-                
-                if message.value.get('username') and message.value.get('new_username'):
-                    old_username, new_username = message.value['username'], message.value['new_username'] 
+                current_app.logger.info(
+                    "Received message with username : %s", message.value["username"]
+                )
+
+                if message.value.get("username") and message.value.get("new_username"):
+                    old_username, new_username = (
+                        message.value["username"],
+                        message.value["new_username"],
+                    )
                     profile_service.update_username(old_username, new_username)
-                elif message.value.get('username'):
+                elif message.value.get("username"):
                     profile_service.create_profile(message.value["username"])
                 else:
                     current_app.logger.error("empty message received.")
 
     def create_topic(self):
         admin_client = KafkaAdminClient(
-            bootstrap_servers=config.KAFKA_1,
-            client_id='test'
+            bootstrap_servers=config.KAFKA_1, client_id="test"
         )
-        topic = NewTopic(name=config.KAFKA_TOPIC, num_partitions=1, replication_factor=1)
+        topic = NewTopic(
+            name=config.KAFKA_TOPIC, num_partitions=1, replication_factor=1
+        )
         try:
             admin_client.create_topics(new_topics=[topic], validate_only=False)
         except Exception as e:
